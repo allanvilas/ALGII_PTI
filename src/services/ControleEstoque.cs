ef905d216fbad85ed3590fdda1a220e6e2929f76
Allan Vilas <48562415+allanvilas@users.noreply.github.com>
2024-02-11 14:55:54 +0000

    Executando na versão dotnet sdk 7.0

--- src/services/ControleEstoque.cs
+++ src/services/ControleEstoque.cs
@@ -1,19 +1,43 @@
 namespace PTI;
 
-public class ControleEstoque
+public static class ControleEstoque
 {
-    private readonly string? Menu;
+    private static readonly string? Menu;
 
-    public ControleEstoque(){
+    private static readonly int[] PossiveisOpcoes = {0,1,2,3,4,5};
+
+    static ControleEstoque(){
         // Construção menu        
+        Menu += "----------[Fim operação]----------\n";
+        Menu += "----------[Inicio operação]----------\n";
         Menu += "[1] Novo\n";
         Menu += "[2] Listar Produtos\n";
         Menu += "[3] Remover Produtos\n";
         Menu += "[4] Entrada Estoque\n";
         Menu += "[5] Saída Estoque\n";
         Menu += "[0] Sair\n";
-
         
     }
+    public static void MostraMenu(int opcao = 999){
+        
+              
+        if(opcao == 0){Environment.Exit(1);}
 
+        try
+        {
+            Console.Write($"{Menu}\nEscolha a opção\n> ");
+            opcao = Convert.ToInt16(Console.ReadLine());
+            
+            if(!PossiveisOpcoes.Contains(opcao)){throw new IndexOutOfRangeException("Você escolheu uma opção não possível, tente novamente.");};
+
+            MostraMenu(opcao);
+        }
+        catch (System.Exception Erro)
+        {   
+            Console.Write($"Não foi possível executar a operação, tente novamente\n Erro:{Erro.Message}\nAperte enter para continuar...\n");            
+            Console.ReadLine();
+            MostraMenu(opcao);
+            throw;
+        }
+    }
 }
